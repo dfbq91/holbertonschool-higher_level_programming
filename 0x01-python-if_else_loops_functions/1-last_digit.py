b6f80a71a8b2287c2e323b9d1fcf576a6eb5a2c6
dfbq91 <dfbq91@gmail.com>
2019-12-10 12:53:33 -0500

    string format set

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 0:
+if number >= 0:
     mod = number % 10
 elif number < 0:
     mod = number % -10
@@ -13,4 +13,4 @@ elif mod == 0:
 elif mod < 6:
     str = 'and is less than 6 and not 0'
 
-print('Last digit of', number, 'is', mod, str)
+print("Last digit of {} {} {} {}".format(number, 'is', mod, str))
