9f78773fc660a525f4001361bbbf530474f2a948
Diego <dfbq91@gmail.com>
2019-12-12 23:35:50 +0000

    calculator task

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    first = int(sys.argv[1])
+    second = int(sys.argv[3])
+
+    if sys.argv[2] == '+':
+        print(first, '+', second, '=', add(first, second))
+
+    elif sys.argv[2] == '-':
+        print(first, '-', second, '=', sub(first, second))
+
+    elif sys.argv[2] == '*':
+        print(first, '*', second, '=', mul(first, second))
+
+    elif sys.argv[2] == '/':
+        print(first, '/', second, '=', div(first, second))
+
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
