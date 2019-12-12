40f64fbe30ca7a655fd8bd392ebe9a3461c64d77
Diego <dfbq91@gmail.com>
2019-12-12 23:59:07 +0000

    fixing

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,13 +3,14 @@ from calculator_1 import add, sub, mul, div
 import sys
 if __name__ == "__main__":
 
-    if len(sys.argv) < 4:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     first = int(sys.argv[1])
     second = int(sys.argv[3])
     op = sys.argv[2]
+
     if sys.argv[2] == '+':
         print("{} {} {} = {}".format(first, op, second, add(first, second)))
 
