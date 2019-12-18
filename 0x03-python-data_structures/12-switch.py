3e22172da6e3e8966e9e7e0b3ce0c30ee98ca17d
Diego <dfbq91@gmail.com>
2019-12-18 00:09:01 +0000

    another tasks

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 a = 89
 b = 10
-b, a = 89, 10
+b, a = b, a
 print("a={:d} - b={:d}".format(a, b))
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *currentA;
+	listint_t *currentB;
+	listint_t *temp;
+	int nnodes = 0;
+	int i = 0;
+
+	if (*head == NULL)
+		return (1);
+
+	currentA = *head;
+	currentB = *head;
+	temp = *head;
+
+	currentB = currentB->next;
+
+	while (currentB->next != NULL)
+	{
+		currentB = currentB->next;
+		temp = temp->next;
+		nnodes++;
+	}
+
+	for (i = 0; i < nnodes / 2; i++)
+	{
+		if (currentA->n != currentB->n)
+			return (0);
+		if (currentA->n == currentB->n)
+		{
+			currentA = currentA->next;
+			currentB = temp;
+			temp = currentA;
+		}
+		while (temp->next != currentB)
+			temp = temp->next;
+		i++;
+
+	}
+	return (1);
+
+}
