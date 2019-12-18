eded8712ec510d80d4bebd314aa59e16b56b1fec
dfbq91 <dfbq91@gmail.com>
2019-12-18 00:14:28 -0500

    fix in spaces

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,10 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in range(len(matrix)):
+            t = 0
             for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), sep=' ,', end=' ')
+                print("{:d}".format(matrix[i][j]), end=''),
+                t = t + 1
+                if t != len(matrix[i]):
+                    print(" ", end='')
             print()
