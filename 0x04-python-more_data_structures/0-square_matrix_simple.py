e963ef374f873549188c8015a72d5425d9c726ef
dfbq91 <dfbq91@gmail.com>
2019-12-19 12:03:49 -0500

    task with dict, lists and sets

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix:
+        newlist = []
+        for i in range(len(matrix)):
+            newlist.append(list(map(lambda x: x ** 2, matrix[i])))
+        return newlist
