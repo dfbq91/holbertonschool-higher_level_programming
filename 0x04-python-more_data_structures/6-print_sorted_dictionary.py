7ab09dc002b8c89229257b5f503e6e92ce6f3a28
dfbq91 <dfbq91@gmail.com>
2019-12-19 12:21:13 -0500

    use copy to unmodify list

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    [print(key, ":", value) for (key, value) in sorted(a_dictionary.items())]
+    newdic = a_dictionary.copy()
+    [print(key, ":", value) for (key, value) in sorted(newdic.items())]
