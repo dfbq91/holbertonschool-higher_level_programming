e8868ba59630e23fce10a583b6e7c813bf66e634
Diego <dfbq91@gmail.com>
2019-12-20 15:06:15 +0000

    fix for values

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,9 @@
 def best_score(a_dictionary):
     if a_dictionary:
         newdic = a_dictionary.copy()
-        return max(newdic)
+        biggest = 0
+        for i in newdic:
+            if newdic[i] > biggest:
+                biggest = newdic[i]
+                bestscore = i
+        return bestscore
