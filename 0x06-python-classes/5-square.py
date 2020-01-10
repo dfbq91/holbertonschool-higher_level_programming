ea74a838ae5a84e3f1633f2c979f223c5e37ca2d
dfbq91 <dfbq91@gmail.com>
2020-01-09 21:51:03 -0500

    area with #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''0-square.py: Create a square class with size and area'''
+
+
+class Square:
+    '''class Square'''
+    def __init__(self, size=0):
+        '''Will initialize square class with size'''
+        self.__size = size
+
+    @property
+    def size(self):
+        '''Returns the size of a square'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''Defines value of size of square and checks if it's > 0 and an int'''
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        '''return current area of a square'''
+        return self.__size * self.__size
+
+    def my_print(self):
+        '''print area of square with #'''
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print('#', end="")
+                print()
