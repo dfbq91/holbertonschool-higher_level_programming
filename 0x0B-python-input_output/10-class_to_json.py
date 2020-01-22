9af918f71f746ca73adbfabc6031622b8e126e09
dfbq91 <dfbq91@gmail.com>
2020-01-21 21:07:03 -0500

    new fix

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''json tasks'''
+
+
+import json
+
+
+def class_to_json(obj):
+    return obj.__dict__
--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''adds all arguments to a Python list, and then save them to a file'''
 
-import json
 import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
@@ -10,7 +9,10 @@ load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 filename = 'add_item.json'
 my_list = []
 
-my_list = load_from_json_file("add_item.json")
-for argument in sys.argv[1:]:
-    my_list.append(argument)
-save_to_json_file(my_list, filename)
+try:
+    my_list = load_from_json_file("add_item.json")
+    for argument in sys.argv[1:]:
+        my_list.append(argument)
+    save_to_json_file(my_list, filename)
+except:
+    save_to_json_file(my_list, filename)
