eae27a334ff57be6e0fd6db27d577261af92d3e2
dfbq91 <dfbq91@gmail.com>
2020-03-02 18:57:16 -0500

    change in order

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
  -- Lists all records of the table second_table
  -- Don’t list rows without a name value
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC, name;
