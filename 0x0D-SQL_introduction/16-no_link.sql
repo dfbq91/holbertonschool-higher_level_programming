651fd20506090fb2c502763fe7350ab530be1d31
dfbq91 <dfbq91@gmail.com>
2020-03-02 18:48:19 -0500

    another fix

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,3 @@
- -- Lists all records of the table second_table of the database hbtn_0c_0
-SELECT `score`, `name`
-FROM `second_table`
-WHERE `name` != ""
-ORDER BY `score` DESC;
+ -- Lists all records of the table second_table
+ -- Don’t list rows without a name value
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
