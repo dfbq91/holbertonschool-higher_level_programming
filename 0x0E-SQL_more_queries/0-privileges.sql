385159322aa8a0b61f90dfac6147650f310da37e
dfbq91 <dfbq91@gmail.com>
2020-03-03 12:41:12 -0500

    privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Lists all privileges of the MySQL users.
+-- Lists privileges of MySQL users user_0d_1 and user_0d_2
+-- using GRANTS
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
