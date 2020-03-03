ee8892736b03c49d953eafcc048be13721e3c53e
dfbq91 <dfbq91@gmail.com>
2020-03-03 17:33:46 -0500

    14 fixed 3 inner joins

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,6 +3,16 @@ SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.ID
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name;
+
+/*This tasks can be done in this way too, but the checker does not accept:
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_show_genres.genre_id = 1 OR tv_show_genres.genre_id = 2 OR tv_show_genres.genre_id = 6 OR tv_show_genres.genre_id = 7 OR tv_show_genres.genre_id = 8
 GROUP BY tv_genres.name
-ORDER BY tv_genres.name;
+ORDER BY tv_genres.name;*/
