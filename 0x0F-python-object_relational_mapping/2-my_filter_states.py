3d452f07a9f2bf8d3fdf75e995bc1a48894a26e1
dfbq91 <dfbq91@gmail.com>
2020-03-28 13:30:29 -0500

    fixes using binary

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,15 +5,15 @@
 import MySQLdb
 from sys import argv
 
-
-db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                     passwd=argv[2], db=argv[3])
-cur = db.cursor()
-m = "SELECT * FROM states WHERE name LIKE '{}'\
-ORDER BY id ASC".format(argv[4])
-cur.execute(m)
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    m = "SELECT * FROM states WHERE name LIKE BINARY '{}'\
+    ORDER BY id ASC".format(argv[4])
+    cur.execute(m)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
     command = argv[4]
-    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY\
-    id ASC", (argv[4],))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s\
+    ORDER BY id ASC", (argv[4],))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
