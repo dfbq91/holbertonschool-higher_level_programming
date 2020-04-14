78294f23dc1c7e7a10243745d671aad19dc8f893
dfbq91 <dfbq91@gmail.com>
2020-04-14 16:23:54 -0500

    more tasks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # Fetches https://intranet.hbtn.io/status
 
-import urllib.request
-
 if __name__ == "__main__":
     import urllib.request
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Takes in a URL, sends a request to the URL and displays
+# the body of the response (decoded in utf-8).
+
+if __name__ == "__main__":
+    from sys import argv
+    from urllib import request, error
+
+    req = request.Request(argv[1])
+    try:
+        with request.urlopen(req) as response:
+            html = response.read()
+            decoded_html = html.decode('utf-8')
+            print(decoded_html)      
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
