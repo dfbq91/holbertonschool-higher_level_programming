45de5d3ac2837283c9a18c88b7340f56127ef82f
dfbq91 <dfbq91@gmail.com>
2020-04-15 11:57:22 -0500

    git tasks to get commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# list 10 commits (from the most recent to oldest)
+# of the repository “rails” by the user “rails”
+
+if __name__ == "__main__":
+    '''Github api'''
+    import requests
+    from sys import argv
+
+    repo = argv[1]
+    owner = argv[2]
+
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'.
+                     format(owner, repo), {'per_page': 10})
+    jsoned = r.json()
+    for i in jsoned:
+        try:
+            print("{}: {}".format(i['sha'], i['commit']['author']['name']))
+        except KeyError:
+            print("None")
