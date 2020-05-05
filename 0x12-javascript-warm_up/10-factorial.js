7d855c52611e7fae12119f747be1ec831f59ccb3
dfbq91 <dfbq91@gmail.com>
2020-05-05 17:47:40 -0500

    semi fixed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@ function factorial (a) {
   if (a <= 1 || isNaN(a)) {
     return 1;
   }
-  return a * factorial(a - 1)
+  return a * factorial(a - 1);
 }
 const myNum = parseInt(process.argv[2]);
 const result = factorial(myNum);
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
   console.log(0);
+} else {
+  process.argv.sort(function (a, b) {
+    return a - b;
+  });
+  console.log(process.argv[process.argv.length - 2]);
 }
-process.argv.sort(function(a, b) {
-  return a - b;
-})
-console.log(process.argv[process.argv.length - 2])
